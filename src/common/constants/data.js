4c043331d715b3193144788cfd2a8ca58fd81304
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-06-01 17:00:39 +0300

    create data.js file with tariffs info array

---
+++ src/common/constants/data.js
@@ -0,0 +1,26 @@
+export const tariffs = [
+  {
+    id: 1,
+    name: "Безлимитный 300",
+    price: 300,
+    speedLimit: 10,
+  },
+  {
+    id: 2,
+    name: "Безлимитный 450",
+    price: 450,
+    speedLimit: 50,
+  },
+  {
+    id: 3,
+    name: "Безлимитный 550",
+    price: 550,
+    speedLimit: 100,
+  },
+  {
+    id: 4,
+    name: "Безлимитный 1000",
+    price: 1000,
+    speedLimit: 200,
+  },
+];
