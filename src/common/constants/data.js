d860083cba01e001bce038af947e6bc9a616611b
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-06-01 22:32:36 +0300

    add category to tariffs array

--- src/common/constants/data.js
+++ src/common/constants/data.js
@@ -4,23 +4,27 @@ export const tariffs = [
     name: "Безлимитный 300",
     price: 300,
     speedLimit: 10,
+    category: "basic",
   },
   {
     id: 2,
     name: "Безлимитный 450",
     price: 450,
     speedLimit: 50,
+    category: "standard",
   },
   {
     id: 3,
     name: "Безлимитный 550",
     price: 550,
     speedLimit: 100,
+    category: "premium",
   },
   {
     id: 4,
     name: "Безлимитный 1000",
     price: 1000,
     speedLimit: 200,
+    category: "ultimate",
   },
 ];
