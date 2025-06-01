291a396f0fab77595bdf91fa62f6acace82b2086
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-06-01 21:37:12 +0300

    add h1 title for the mainBlock

--- src/components/mainBlock/MainBlock.jsx
+++ src/components/mainBlock/MainBlock.jsx
@@ -4,6 +4,7 @@ import styles from "./MainBlock.module.scss";
 export const MainBlock = () => {
   return (
     <main className={styles.main}>
+      <h1 className={styles.title}>Наши Тарифы</h1>
       <TariffCards />
     </main>
   );
